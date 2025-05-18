e781adfe98216ab4a3382010057673d1d4c0ab61
userzenox <piyush.132kp@gmail.com>
2025-05-18 20:50:20 +0530

    commit-18

---
+++ MediumProblems/75_Sort_Class.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sortColors(vector<int>& nums) {
+    int low = 0, mid = 0, high = nums.size() - 1;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        } else if (nums[mid] == 1) {
+            mid++;
+        } else { // nums[mid] == 2
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+}
+
+int main() {
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+
+    cout << "Before sorting: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    sortColors(nums);
+
+    cout << "After sorting:  ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+
